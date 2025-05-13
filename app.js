127438d43c08a5e205a4faa405ad40b269b4e174
subhadip manna <61026936+subhadipmanna@users.noreply.github.com>
2025-05-14 03:06:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3001; // Must match frontend's API_URL port
 // CORS configuration for local development
 app.use(cors({
   origin: [
-    'http://localhost:5173', // Alternative localhost
+    'https://amitportfolio.vercel.app', // Alternative localhost
    
   ],
   methods: ['POST', 'GET'],
